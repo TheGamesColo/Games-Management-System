58fd82b84da45458313d0abaa24bcefb6c8b602e
TheGamesColo <112554654+TheGamesColo@users.noreply.github.com>
2025-10-07 06:39:36 +0200

    Changes related to adding game form
    - Adapted code to read URL with queries.
    - Added code to insert/update records in the database.

--- server.mjs
+++ server.mjs
@@ -12,11 +12,37 @@ const connection = await mysql.createConnection({host: '127.0.0.1', user: 'root'
 const server = createServer(async (req, res) => {
 
     let file = await readPublicFile(req);
+    let url = req.url.split('?');
 
-    if (req.url === '/api/games_list') {
+    if (url[0] === '/api/games_list') {
         const [data] = await connection.execute('SELECT * FROM games');
         res.writeHead(200, {'Content-Type': 'application/json'});
         res.end(JSON.stringify(data));
+    } else if (url[0] === '/api/submit' && req.method === 'POST') {
+        let body = '';
+
+        req.on('data', chunk => {
+            body += chunk.toString();
+        });
+
+        req.on('end', async () => {
+            const data = JSON.parse(body);
+            if (data.img_cover == undefined) data.img_cover = null;
+
+            let result;
+            if (!data.id) {
+                [result] = await connection.execute('INSERT INTO games (title,library,platform,is_physical,status,completion,is_completed,achievs_completed,achievs_all,purchase_date,price,region,comment,rating,img_cover) VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)',
+                    [data.title, data.library, data.platform, data.is_physical, data.status, data.completion, data.is_completed, data.achievs_completed, data.achievs_all, data.purchase_date, data.price, data.region, data.comment, data.rating, data.img_cover]);
+            } else {
+                [result] = await connection.execute('UPDATE games SET title=?,library=?,platform=?,is_physical=?,status=?,completion=?,is_completed=?,achievs_completed=?,achievs_all=?,purchase_date=?,price=?,region=?,comment=?,rating=?,img_cover=? WHERE id=?',
+                    [data.title, data.library, data.platform, data.is_physical, data.status, data.completion, data.is_completed, data.achievs_completed, data.achievs_all, data.purchase_date, data.price, data.region, data.comment, data.rating, data.img_cover, data.id]);
+            }
+
+            if (result.affectedRows === 1) {
+                res.writeHead(200, {'Content-Type': 'application/json'});
+                res.end(JSON.stringify({response: 200, id: result.insertId}));
+            }
+        });
     } else {
         if (file) {
             res.writeHead(200, {'Content-Type': file.contentType});
@@ -37,7 +63,7 @@ server.listen(port, hostname, () => {
 async function readPublicFile(request) {
 
     let filePath = request.url.split("?")[0];
-    filePath = filePath === '/' ? '/index.html' : request.url;
+    if (filePath === '/') { filePath = '/index.html'; }
     filePath = path.join(__dirname, 'public', filePath);
 
     const extension = path.extname(filePath);
